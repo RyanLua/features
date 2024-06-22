3880629f8c5338aa4ce965a714552d244f860d21
Ryan Luu <ryan.luu@ryanluu.dev>
2024-06-22 06:57:11 +0000

    Fix bugs

--- src/rojo/install.sh
+++ src/rojo/install.sh
@@ -3,26 +3,23 @@ set -e
 
 echo "Activating feature 'rojo'"
 
+# Make sure we have a temporary directory
+TMPDIR=/home/vscode/tmp
+mkdir -p $TMPDIR
+
 # Install Aftman
 AFTMAN_VERSION=0.3.0
 
 wget https://github.com/LPGhatguy/aftman/releases/download/v$AFTMAN_VERSION/aftman-$AFTMAN_VERSION-linux-x86_64.zip
-unzip aftman-$AFTMAN_VERSION-linux-x86_64.zip
-./aftman self-install
-. ~/.profile
+unzip aftman-$AFTMAN_VERSION-linux-x86_64.zip -d $TMPDIR
+mv $TMPDIR/aftman /usr/local/bin/aftman
 
 # Install Rojo
-if [[ -z "${VERSION}" ]]; then
-ROJO_VERSION="latest"
-else
-ROJO_VERSION="${VERSION}"
-fi
-
-if [ $ROJO_VERSION == "latest" ]
+if [ "${ROJO_VERSION:-latest}" = "latest" ]
 then
     aftman add --global rojo-rbx/rojo
 else
-    aftman add --global rojo-rbx/rojo@${ROJO_VERSION}
+    aftman add --global rojo-rbx/rojo@"${ROJO_VERSION}"
 fi
-aftman trust rojo
+aftman trust rojo-rbx/rojo
 aftman install
