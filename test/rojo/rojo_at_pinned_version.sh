9d7171f91dedab8c78951d9b7d9dd398bd6ba12e
Ryan Luu <ryan.luu@ryanluu.dev>
2024-06-23 02:07:46 +0000

    Add tests for Rojo feature

---
+++ test/rojo/rojo_at_pinned_version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This test file will be executed against one of the scenarios devcontainer.json test that
+# includes the 'rojo' feature with "version": "6.2.0" option.
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "aftman version" aftman  --version
+check "rojo version" rojo  --version
+check "correct rojo version" rojo  --version | grep 6.2.0
+
+# Report results
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
