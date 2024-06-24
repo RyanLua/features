19b597d4fdb9b683444748e5d3093399f8c5bd03
Ryan Luu <ryan.luu@ryanluu.dev>
2024-06-24 23:08:40 +0000

    Fix rojo installing

--- src/rojo/install.sh
+++ src/rojo/install.sh
@@ -13,13 +13,14 @@ AFTMAN_VERSION=0.3.0
 wget https://github.com/LPGhatguy/aftman/releases/download/v$AFTMAN_VERSION/aftman-$AFTMAN_VERSION-linux-x86_64.zip
 unzip aftman-$AFTMAN_VERSION-linux-x86_64.zip -d $TMPDIR
 mv $TMPDIR/aftman /usr/local/bin/aftman
+aftman self-install
 
 # Install Rojo
+aftman trust rojo-rbx/rojo
+
 if [ "${ROJO_VERSION:-latest}" = "latest" ]
 then
     aftman add --global rojo-rbx/rojo
 else
     aftman add --global rojo-rbx/rojo@"${ROJO_VERSION}"
 fi
-aftman trust rojo-rbx/rojo
-aftman install
